23846dcc409fb6cf0e6d7b82614645e859b4eff9
Alberto Huélamo <1891056+alhuelamo@users.noreply.github.com>
2021-03-31 19:06:35 +0200

    Added LinkedList::peek

--- src/collections/linked_list.rs
+++ src/collections/linked_list.rs
@@ -13,35 +13,51 @@ impl<T: fmt::Display> LinkedList<T> {
         LinkedList::Nil
     }
 
-    pub fn new_head(element: T) -> Self {
+    pub fn new(element: T) -> Self {
         LinkedList::Tail { head: element }
     }
 
     pub fn append(self, element: T) -> Self {
         match self {
-            LinkedList::Nil => LinkedList::new_head(element),
+            LinkedList::Nil => LinkedList::new(element),
             LinkedList::Tail { head } => {
-                let new_tail = Box::new(LinkedList::new_head(element));
-                LinkedList::Link { head, tail: new_tail }
-            },
+                let new_tail = Box::new(LinkedList::new(element));
+                LinkedList::Link {
+                    head,
+                    tail: new_tail,
+                }
+            }
             LinkedList::Link { head, tail } => {
                 let new_tail = Box::new(tail.append(element));
-                LinkedList::Link { head, tail: new_tail }
+                LinkedList::Link {
+                    head,
+                    tail: new_tail,
+                }
             }
         }
     }
 
-   fn to_string(&self) -> String {
-       format!("List({})", self.str_loop("".to_string()))
-   }
+    pub fn peek(&self, index: usize) -> Result<&T, &'static str> {
+        match self {
+            LinkedList::Nil => Err("Cannot peek anything from an empty list."),
+            LinkedList::Tail { head } if index == 0 => Ok(head),
+            LinkedList::Tail { .. } => Err("Index overflow."),
+            LinkedList::Link { head, .. } if index == 0 => Ok(head),
+            LinkedList::Link { tail, ..} => tail.peek(index - 1),
+        }
+    }
 
-   fn str_loop(&self, acc: String) -> String {
-       match self {
-           LinkedList::Nil => acc,
-           LinkedList::Tail { head } => format!("{}, {})", acc, head),
-           LinkedList::Link { head, tail } => tail.str_loop(format!("{}, {}", acc, head)),
-       }
-   }
+    fn to_string(&self) -> String {
+        format!("List({})", self.str_loop("".to_string()))
+    }
+
+    fn str_loop(&self, acc: String) -> String {
+        match self {
+            LinkedList::Nil => acc,
+            LinkedList::Tail { head } => format!("{}, {})", acc, head),
+            LinkedList::Link { head, tail } => tail.str_loop(format!("{}, {}", acc, head)),
+        }
+    }
 }
 
 impl<T: fmt::Display> fmt::Debug for LinkedList<T> {
@@ -61,7 +77,10 @@ mod test_linked_list {
 
     #[test]
     fn test_new_head() {
-        assert_eq!(LinkedList::<u8>::new_head(10 as u8), LinkedList::<u8>::Tail { head: 10 as u8 })
+        assert_eq!(
+            LinkedList::<u8>::new(10 as u8),
+            LinkedList::<u8>::Tail { head: 10 as u8 }
+        )
     }
 
     #[test]
@@ -73,7 +92,7 @@ mod test_linked_list {
 
     #[test]
     fn test_append_from_tail() {
-        let head = LinkedList::new_head(10 as u8);
+        let head = LinkedList::new(10 as u8);
 
         let expected = LinkedList::Link {
             head: 10 as u8,
@@ -89,17 +108,34 @@ mod test_linked_list {
             head: 10 as u8,
             tail: Box::new(LinkedList::Tail { head: 20 as u8 }),
         };
-        
+
         let expected = LinkedList::Link {
             head: 10 as u8,
-            tail: Box::new(
-                LinkedList::Link {
-                    head: 20 as u8,
-                    tail: Box::new(LinkedList::Tail { head: 30 }),
-                }
-            ),
+            tail: Box::new(LinkedList::Link {
+                head: 20 as u8,
+                tail: Box::new(LinkedList::Tail { head: 30 }),
+            }),
         };
 
         assert_eq!(list.append(30), expected);
+    }
+
+    #[test]
+    fn test_peek() {
+        let list = LinkedList::<u8>::new(9 as u8).append(42);
+        assert_eq!(list.peek(0).unwrap(), &(9 as u8));
+        assert_eq!(list.peek(1).unwrap(), &(42 as u8));
+    }
+
+    #[test]
+    fn test_peek_nil() {
+        let list = LinkedList::<u8>::Nil;
+        assert_eq!(list.peek(0), Err("Cannot peek anything from an empty list."))
+    }
+
+    #[test]
+    fn test_peek_overflow() {
+        let list = LinkedList::new(9).append(42);
+        assert_eq!(list.peek(2), Err("Index overflow."))
     }
 }
